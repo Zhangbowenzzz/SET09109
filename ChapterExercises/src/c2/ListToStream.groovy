811324d89823d6ee6844e00378be497867e450e6
hintofbasil <hintofbasil@github.com>
2017-01-27 22:31:27 +0000

    Completed 2-2

--- ChapterExercises/src/c2/ListToStream.groovy
+++ ChapterExercises/src/c2/ListToStream.groovy
@@ -3,15 +3,18 @@ package c2
 import org.jcsp.lang.*
 
 class ListToStream implements CSProcess{
-	
-	def ChannelInput inChannel
-	def ChannelOutput outChannel
-	
-	void run (){
-		def inList = inChannel.read()
-		while (inList[0] != -1) {
-			// hint: output	list elements as single integers
-		}
-		outChannel.write(-1)
-	}
+
+  def ChannelInput inChannel
+  def ChannelOutput outChannel
+
+  void run (){
+    def inList = inChannel.read()
+    while (inList[0] != -1 && inList[1] != -1 && inList[2] != -1) {
+      for(int i in 0 .. 2) {
+        outChannel.write(inList[i]);
+      }
+      inList = inChannel.read();
+    }
+    outChannel.write(-1)
+  }
 }
