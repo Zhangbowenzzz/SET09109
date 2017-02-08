cf8717634d2704e5859b01a38492177aaa2ec8ca
hintofbasil <hintofbasil@github.com>
2017-02-08 13:06:50 +0000

    Implemented 4-2

--- ChapterExercises/src/c4/ResetNumbers.groovy
+++ ChapterExercises/src/c4/ResetNumbers.groovy
@@ -1,31 +1,33 @@
 package c4
 
-import org.jcsp.lang.*
 import org.jcsp.groovy.*
 import org.jcsp.groovy.plugAndPlay.*
+import org.jcsp.lang.*
 
 
 class ResetNumbers implements CSProcess {
-  
+
   def ChannelOutput outChannel
   def ChannelInput resetChannel
   def int initialValue = 0
-  
+
   void run() {
-    
+
     One2OneChannel a = Channel.createOne2One()
     One2OneChannel b = Channel.createOne2One()
     One2OneChannel c = Channel.createOne2One()
-    
-    def testList = [ new GPrefix ( prefixValue: initialValue, 
-                                   outChannel: a.out(), 
+
+    def testList = [ new GPrefix ( prefixValue: initialValue,
+                                   outChannel: a.out(),
                                    inChannel: c.in() ),
-                     new GPCopy ( inChannel: a.in(), 
-                            	  outChannel0: outChannel, 
-                            	  outChannel1: b.out() ),
-                     // requires a constructor for ResetSuccessor
+                     new GPCopy ( inChannel: a.in(),
+                                  outChannel0: outChannel,
+                                  outChannel1: b.out() ),
+                     new ResetSuccessor ( resetChannel: resetChannel,
+                                          inChannel: b.in(),
+                                          outChannel: c.out() ),
                   ]
-    new PAR ( testList ).run()    
+    new PAR ( testList ).run()
   }
 }
-                         
+
--- ChapterExercises/src/c4/ResetSuccessor.groovy
+++ ChapterExercises/src/c4/ResetSuccessor.groovy
@@ -1,21 +1,28 @@
 package c4
 
 
-import org.jcsp.lang.*
 import org.jcsp.groovy.*
+import org.jcsp.lang.*
 
 class ResetSuccessor implements CSProcess {
-	  
+
   def ChannelOutput outChannel
   def ChannelInput  inChannel
   def ChannelInput  resetChannel
-	  
+
   void run () {
     def guards = [ resetChannel, inChannel  ]
     def alt = new ALT ( guards )
-	while (true) {
-	  // deal with inputs from resteChannel and inChannel
-	  // use a priSelect
-	}
+    while (true) {
+      def index = alt.priSelect();
+      switch( index ) {
+      case 0:
+        inChannel.read();
+        outChannel.write( resetChannel.read() );
+        break;
+      case 1:
+        outChannel.write( inChannel.read() + 1 );
+      }
+    }
   }
 }
